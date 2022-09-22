501293ae838331f712c41ce4561a0d5ee2076cb3
Bhagyashree Jena <73177843+bhagyashree-j@users.noreply.github.com>
2022-09-22 13:41:22 +0530

    Initial commit

---
+++ module/Album/src/Album/Form/AlbumForm.php
@@ -0,0 +1,110 @@
+<?php
+namespace Album\Form;
+
+ use Zend\Form\Form;
+ use Zend\Form\Element;
+ 
+
+ class AlbumForm extends Form
+ {
+     public function __construct($name = null)
+     {
+         // we want to ignore the name passed
+         parent::__construct('album');
+
+         $this->add(array(
+             'name' => 'id',
+             'type' => 'Hidden',
+         ));
+         $this->add(array(
+             'name' => 'title',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Title',
+             ),
+         ));
+         $this->add(array(
+             'name' => 'artist',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Artist',
+             ),
+         ));
+         $this->add(array(
+            'name' => 'email',
+            'type' => 'Zend\Form\Element\Email',
+            'options' => array(
+                'label' => 'Email',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'release_date',
+            'type' => Element\Date::class,
+            'options' => array(
+                'label' => 'Release Date',
+                'format' => 'Y-m-d'
+            ),
+        ));
+         $this->add(array(
+             'name' => 'submit',
+             'type' => 'Submit',
+             'attributes' => array(
+                 'value' => 'Go',
+                 'id' => 'submitbutton',
+             ),
+         ));
+     }
+     
+ }
+
+ class AlbumForm1 extends Form
+ {
+     public function __construct($name = null)
+     {
+         // we want to ignore the name passed
+         parent::__construct('album');
+
+         $this->add(array(
+             'name' => 'id',
+             'type' => 'Hidden',
+         ));
+         $this->add(array(
+             'name' => 'title',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Title',
+             ),
+         ));
+         $this->add(array(
+             'name' => 'artist',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Artist',
+             ),
+         ));
+         $this->add(array(
+            'name' => 'email',
+            'type' => 'Zend\Form\Element\Email',
+            'options' => array(
+                'label' => 'Email',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'release_date',
+            'type' => Element\Date::class,
+            'options' => array(
+                'label' => 'Release Date',
+                'format' => 'Y-m-d'
+            ),
+        ));
+         $this->add(array(
+             'name' => 'submit',
+             'type' => 'Submit',
+             'attributes' => array(
+                 'value' => 'Go',
+                 'id' => 'submitbutton',
+             ),
+         ));
+     }
+     
+ }
